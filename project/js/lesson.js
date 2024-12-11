45a6a8a44537c7a705572fff46f479d079982565
maliika67 <109336557+maliika67@users.noreply.github.com>
2024-12-12 00:34:38 +0600

    Add files via upload

---
+++ project/js/lesson.js
@@ -0,0 +1,15 @@
+const phoneInput = document.querySelector('#phone_input');
+const phoneButton = document.querySelector('#phone_button');
+const phoneResult = document.querySelector('#phone_result');
+
+const regExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
+
+phoneButton.onclick = () => {
+    if(regExp.test(phoneInput.value)){
+        phoneInput.innerHTML = "OK"
+        phoneResult.style.color = "green"
+    }else{
+        phoneResult.innerHTML = "Invalid phone number"
+        phoneResult.style.color = "red"
+    }
+}
